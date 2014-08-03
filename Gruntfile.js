df1ba9a7175cd0740a9f86b7a0bafc2d8eaa5414
Jessica Vreeland <jessicavreeland@gmail.com>
2014-08-02 19:41:58 -0700

    sandbox

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/frog/**/*.js',
+                dest: 'build/frog.min.js'
+            }
+        },
+        ng_boba: {
+            build: {
+                src: 'src/frog/**/*.js',
+                dest: 'build/frog.min.js'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-ng-boba');
+
+
+    // Default task(s).
+    grunt.registerTask('default', ['ng_boba']);
+
+};
---
+++ src/frog/module1.js
@@ -0,0 +1,1 @@
+angular.module('frog').controller('module1');
