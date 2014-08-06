f9379be487ce890ec3fc650bf406850d2985b284
Jessica Vreeland <jessicavreeland@gmail.com>
2014-08-05 22:39:57 -0700

    Pass in module name

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         ng_boba: {
             options: {
-                module: 'jModule'
+                modules: ['frog']
             },
             build: {
                 src: 'src/frog/**/*.js',
